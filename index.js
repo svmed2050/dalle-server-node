10ccc7abd3c815c8d7a9456a3ca9dc19674ddd12
Pavel <pavelsergienko7@gmail.com>
2023-04-03 19:20:43 +0300

    remove env cached

--- index.js
+++ index.js
@@ -9,7 +9,17 @@ import dalleRoutes from './routes/dalleRoutes.js'
 dotenv.config()
 
 const app = express()
-app.use(cors())
+
+// app.use(cors())
+const corsOptions = {
+	origin: 'http://localhost:5173',
+	credentials: true, //access-control-allow-credentials:true
+	optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+
+///////////////
+
 app.use(express.json({ limit: '50mb' }))
 
 app.use('/api/v1/post', postRoutes)
